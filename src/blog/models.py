bec3ea16818cdb7e26d490fb253b8b31fc7dbb5b
Konstantin Mosin <KonstantinMosin@coherentsolutions.com>
2023-07-23 19:10:59 +0300

    fill home page via posts

--- src/blog/models.py
+++ src/blog/models.py
@@ -14,5 +14,7 @@ class Post(models.Model):
     created_at = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    objects = models.Manager()
+
     def __str__(self) -> str:
         return self.title
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
+from .models import Post
 
 
 class MainView(View):
     @staticmethod
     def get(request, *args, **kwargs):
+        posts = Post.objects.order_by('-id').all()
+
+        paginator = Paginator(posts, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         return render(
             request,
             'blog/home.html',
+            context={'page_obj': page_obj}
         )
